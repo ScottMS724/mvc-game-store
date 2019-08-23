aab7ed0ce046bd5a23cb923ad7c31c1715aa8af5
Scott Silverman <scottms724@gmail.com>
2019-08-23 20:44:52 +0000

    Debugging users_controller to fix sessions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,51 +1,54 @@
 class UsersController < ApplicationController 
   
   get '/signup' do
-    if !logged_in?
+    if session[:user_id].nil?
       erb :'users/new'
     else
-      redirect '/games'
+      redirect to '/games'
     end
   end
   
   post '/signup' do 
-    @user = User.new(username: params["username"], password: params["password"])
-
-    if @user.save && !@user.username.empty?
-      @user.save
+   if !params["username"].empty? && !params["password"].empty?
+      @user = User.create(params)
       session[:user_id] = @user.id
-      redirect "/games"
+
+       redirect to '/games'
     else
-      redirect "/signup"
+      redirect to '/signup'
     end
   end
 
   get '/login' do 
-    if !logged_in?
-      erb :'users/login'
+    if logged_in?
+      redirect to '/games'
     else
-      redirect '/games'
+      erb :'/users/login'
     end
   end
 
   post '/login' do
     @user = User.find_by(:username => params[:username])
-    if @user && @user.authenticate(params[:password])
+    if @user != nil && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect "/games"
+
+       redirect to '/games'
     else
-      redirect "/login?error=Invalid name or password."
+      redirect to '/login'
     end
   end
 
   get '/logout' do
-    session.clear 
-    redirect '/login'
+    if logged_in?
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
   end 
   
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
-    @games =  @user.games 
     erb :'/users/show'
   end
   
